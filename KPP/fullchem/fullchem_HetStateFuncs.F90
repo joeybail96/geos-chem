1b4bf025f48d707a57e8cc9cf81061079afddf9e
joeybail96 <joey.bail@utah.edu>
2025-06-29 15:46:30 -0600

    adding debugging print statements

--- KPP/fullchem/fullchem_HetStateFuncs.F90
+++ KPP/fullchem/fullchem_HetStateFuncs.F90
@@ -207,9 +207,21 @@ CONTAINS
     H%H_conc_PLYA4 = 10.0**( -5.0_dp              )
 
     ! alkalinity fraction in playa dust
-    H%f_Alk_PLYA1   = SafeDiv( State_Chm%Species(id_PLYAAL1)%Conc(I,J,L),     &
-                               State_Chm%Species(id_PLYA1  )%Conc(I,J,L),     &
-                               0.0_dp                                       )
+    PRINT *, 'id_PLYA1 = ', id_PLYA1
+    PRINT *, 'id_PLYAAL1 = ', id_PLYAAL1
+    PRINT *, 'State_Chm%nSpecies = ', State_Chm%nSpecies
+    
+    IF (ASSOCIATED(State_Chm%Species(id_PLYAAL1)%Conc)) THEN
+        PRINT *, 'PLYAAL1 Conc:', State_Chm%Species(id_PLYAAL1)%Conc(I,J,L)
+    ELSE
+        PRINT *, 'PLYAAL1 Conc NOT associated'
+    ENDIF
+    
+    IF (ASSOCIATED(State_Chm%Species(id_PLYA1)%Conc)) THEN
+        PRINT *, 'PLYA1 Conc:', State_Chm%Species(id_PLYA1)%Conc(I,J,L)
+    ELSE
+        PRINT *, 'PLYA1 Conc NOT associated'
+    ENDIF
 
     H%f_Alk_PLYA2   = SafeDiv( State_Chm%Species(id_PLYAAL2)%Conc(I,J,L),     &
                                State_Chm%Species(id_PLYA2  )%Conc(I,J,L),     &
